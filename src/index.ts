82f8e47dd22020a0eaa61fa0c1764e138575b66a
Andrey Sholinkka <sholinka2111@gmail.com>
2020-10-22 19:24:31 +0700

    init endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 // import { Customer, CustomerType } from './mongodb'
 import { Customer, CustomerType } from './mongoose'
+import { Account, AccountType } from './account'
+import { Transaction, TransactionType } from './transaction'
 
 async function initApp() {
   const app = express()
@@ -22,6 +24,9 @@ async function initApp() {
   //init db mongose
   await mongoose.connect(`${process.env.MONGODB_URI}`, { useUnifiedTopology: true })
   const CustomerModel = new Customer()
+  const AccountModel = new Account()
+  const TransactionModel = new Transaction()
+
 
   app.use(bodyParser.json())
 
@@ -85,6 +90,127 @@ async function initApp() {
 
     res.send({ success: true })
   })
+
+  // CRUD CUSTOMERS
+  app.post('/account', async function (req, res, next) {
+    try {
+      await AccountModel.create(req.body)
+    } catch (error) {
+      return next(error)
+    }
+    res.send({ success: true })
+  })
+
+  app.get('/account', async function (req, res, next) {
+    let accounts: AccountType[]
+    try {
+      accounts = await AccountModel.getAll()
+    } catch (error) {
+      return next(error)
+    }
+    return res.send(accounts)
+  })
+
+  app.get('/account/:id', async function (req, res, next) {
+    let account: AccountType | null
+    try {
+      account = await AccountModel.getByID(req.params.id)
+    } catch (error) {
+      return next(error)
+    }
+    return res.send(account)
+  })
+
+  app.put('/account/:id', async function (req, res, next) {
+    try {
+      await AccountModel.update(req.params.id, req.body)
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
+  app.delete('/account/:id', async function (req, res, next) {
+    try {
+      await AccountModel.delete(req.params.id)
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
+  app.delete('/account/', async function (req, res, next) {
+    try {
+      await AccountModel.deleteAll()
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
+  // CRUD TRANSACTION
+  app.post('/transaction', async function (req, res, next) {
+    try {
+      await TransactionModel.create(req.body)
+    } catch (error) {
+      return next(error)
+    }
+    res.send({ success: true })
+  })
+
+  app.get('/transaction', async function (req, res, next) {
+    let transactions: TransactionType[]
+    try {
+      transactions = await TransactionModel.getAll()
+    } catch (error) {
+      return next(error)
+    }
+    return res.send(transactions)
+  })
+
+  app.get('/transaction/:id', async function (req, res, next) {
+    let transaction: TransactionType | null
+    try {
+      transaction = await TransactionModel.getByID(req.params.id)
+    } catch (error) {
+      return next(error)
+    }
+    return res.send(transaction)
+  })
+
+  app.put('/transaction/:id', async function (req, res, next) {
+    try {
+      await TransactionModel.update(req.params.id, req.body)
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
+  app.delete('/transaction/:id', async function (req, res, next) {
+    try {
+      await TransactionModel.delete(req.params.id)
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
+  app.delete('/transaction/', async function (req, res, next) {
+    try {
+      await TransactionModel.deleteAll()
+    } catch (error) {
+      return next(error)
+    }
+
+    res.send({ success: true })
+  })
+
 
   app.use(function (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
     res.status(500).send({
